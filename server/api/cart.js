7648a5689ee28771c5394ed7dad06f1a91efc416
Kate Yun <ggobugi27@gmail.com>
2018-06-07 19:25:24 -0400

    update(cart-route) post route updated and working with hard coded LineItem

--- server/api/cart.js
+++ server/api/cart.js
@@ -7,38 +7,45 @@ module.exports = router
 // const cart = JSON.parse(localStorage.getItem('cart'))
 // cart.items = {}
 
-router.post('/:userId/:bookId', (req, res) => {
-
-    User.findById(req.params.userId)
+router.post('/:userId/:bookId', (req, res) => {// creates cart order
+    User.findById(req.params.userId)//check if user exists
     .then(user => {
         if (user) return user
         else res.sendStatus(404);
     })
     .then(user => {
-        Order.findAll({userId: user.id, isCart : true})
+        if (user.id){
+        Order.findOne({ where : {userId: user.id, isCart : true}}) //if user has a cart order
         .then(order => {
-            console.log(order)
+            if (!order) return Order.create({userId : user.id}) // create cart order if there is no cart order
+            else return order
         })
+        .then(order => {
+            // hardcoded but change with data in req.body
+            // error if book does not exist. 
+            LineItem.create({bookId : req.params.bookId, orderId : order.id, price : 1, quantity : 1}).then(() => res.sendStatus(202))
+        })
+        }
     })
 
-    // User.findAll({include:[{model: Order}], where: {id: req.params.userId, isCart: true}})
-    //     .then(user => {
-    //         console.log(user, ',,dlf**********')
-    //         if (user) {
+//     // User.findAll({include:[{model: Order}], where: {id: req.params.userId, isCart: true}})
+//     //     .then(user => {
+//     //         console.log(user, ',,dlf**********')
+//     //         if (user) {
 
-    //             return user
-    //         }
-    //         else {
+//     //             return user
+//     //         }
+//     //         else {
                 
-    //             res.sendStatus(404)
-    //         }
-    //     })
-    // const { bookId } = req.body
-    // if (!cart.items[bookId]) {
-    //     cart.items[bookId] = 1
-    // }
-    // else {
-    //     cart.items[bookId]++
-    // }
-    // res.send(cart.items)
+//     //             res.sendStatus(404)
+//     //         }
+//     //     })
+//     // const { bookId } = req.body
+//     // if (!cart.items[bookId]) {
+//     //     cart.items[bookId] = 1
+//     // }
+//     // else {
+//     //     cart.items[bookId]++
+//     // }
+//     // res.send(cart.items)
 })
