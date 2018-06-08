4da0fa40bbbbf9f7db3f7a6ed082422bbd8cf487
Jacob Feinberg <JacobFeinberg92@gmail.com>
2018-06-08 13:00:32 -0400

    update(cart-route) post route for cart completed

--- server/api/cart.js
+++ server/api/cart.js
@@ -1,53 +1,68 @@
-const router = require('express').Router()
-const { User , Order, LineItem, Book} = require('../db/models')
-module.exports = router
+const router = require("express").Router();
+const { User, Order, LineItem, Book } = require("../db/models");
+const Sequelize = require('sequelize');
+module.exports = router;
 
 // const newCart = {}
 // localStorage.setItem('cart', JSON.stringify(newCart))
 // const cart = JSON.parse(localStorage.getItem('cart'))
 // cart.items = {}
 
-router.get('/:userId/:bookId', (req, res) => {// creates cart order
-    User.findById(req.params.userId)//check if user exists
+router.post("/:userId/:bookId", (req, res) => {
+  // creates cart order
+  User.findById(req.params.userId) //check if user exists
     .then(user => {
-        if (user) return user
-        else res.sendStatus(404);
-    })
-    .then(user => {
-        if (user.id){
-        Order.findOne({ where : {userId: user.id, isCart : true}}) //if user has a cart order
-        .then(order => {
-            if (!order) return Order.create({userId : user.id}) // create cart order if there is no cart order
-            else return order
-        })
-        .then(order => {
-            Book.findById(req.params.bookId)
-            .then(book => {
-                //Creates lineitem with bookId and orderId
-                LineItem.create({bookId : book.id, orderId : order.id, price : book.price, quantity : book.inventoryQuantity}).then(() => res.sendStatus(202))
-            })
-        })
-        }
-    })
+      if (user) {
+        Order.findOne({ where: { userId: user.id, isCart: true } }) //if user has a cart order
+          .then(order => {
+            if (!order) return Order.create({ userId: user.id });
+            // create cart order if there is no cart order
+            else return order;
+          })
+          .then(order => {
+            Book.findById(req.params.bookId).then(book => {
+              //Creates lineitem with bookId and orderId
+              if(book){
+                LineItem.findOne({ where: { orderId: order.id, bookId: book.id } })
+                .then(lineItem => {
+                  if (!lineItem) {
+                    LineItem.create({
+                      bookId: book.id,
+                      orderId: order.id,
+                      price: book.price,
+                      quantity: 1
+                    });
+                  } else {
+                    lineItem.update({
+                      quantity: Sequelize.literal("quantity + 1")
+                    });
+                  }
+                })
+                .then(() => res.sendStatus(202));
+              } else res.sendStatus(404);
+            });
+          });
+      } else res.sendStatus(404);
+    });
 
-//     // User.findAll({include:[{model: Order}], where: {id: req.params.userId, isCart: true}})
-//     //     .then(user => {
-//     //         console.log(user, ',,dlf**********')
-//     //         if (user) {
+  //     // User.findAll({include:[{model: Order}], where: {id: req.params.userId, isCart: true}})
+  //     //     .then(user => {
+  //     //         console.log(user, ',,dlf**********')
+  //     //         if (user) {
 
-//     //             return user
-//     //         }
-//     //         else {
-                
-//     //             res.sendStatus(404)
-//     //         }
-//     //     })
-//     // const { bookId } = req.body
-//     // if (!cart.items[bookId]) {
-//     //     cart.items[bookId] = 1
-//     // }
-//     // else {
-//     //     cart.items[bookId]++
-//     // }
-//     // res.send(cart.items)
-})
+  //     //             return user
+  //     //         }
+  //     //         else {
+
+  //     //             res.sendStatus(404)
+  //     //         }
+  //     //     })
+  //     // const { bookId } = req.body
+  //     // if (!cart.items[bookId]) {
+  //     //     cart.items[bookId] = 1
+  //     // }
+  //     // else {
+  //     //     cart.items[bookId]++
+  //     // }
+  //     // res.send(cart.items)
+});
