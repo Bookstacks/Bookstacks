d5b31418b33e202c99bbc344b4979ad73c0e075f
test <test@tests-MacBook-Air.local>
2018-06-09 10:22:28 -0400

    added seed information in server/index.js

--- server/index.js
+++ server/index.js
@@ -11,6 +11,10 @@ const sessionStore = new SequelizeStore({db})
 const PORT = process.env.PORT || 8080
 const app = express()
 const socketio = require('socket.io')
+
+const Book = require('./db/models/book');
+const Bluebird = require('bluebird')
+
 module.exports = app
 
 /**
@@ -91,7 +95,66 @@ const startListening = () => {
   require('./socket')(io)
 }
 
-const syncDb = () => db.sync()
+// const syncDb = () => db.sync()
+
+var books = [
+
+  {
+      title: "Great Gatsby",
+      author: "F. Scott Fitzgerald",
+      genre: ["Novel", "Historical Fiction"],
+      price: 9, 
+      imageUrl: "/books/great_gatsby.jpeg",
+      description: "The Great Gatsby, F. Scott Fitzgerald’s third book, stands as the supreme achievement of his career. First published in 1925, this quintessential novel of the Jazz Age has been acclaimed by generations of readers. The story of the mysteriously wealthy Jay Gatsby and his love for the beautiful Daisy Buchanan, of lavish parties on Long Island at a time when The New York Times noted “gin was the national drink and sex the national obsession,” it is an exquisitely crafted tale of America in the 1920s.",
+      inventoryQuantity: 10
+  },
+  {
+      title: "Infinite Jest",
+      author: "David Foster Wallace",
+      genre: ["Hysterical realism", "Satire", "Tragicomedy", "Post-postmodernism", "Encyclopedic novel"],
+      price: 11, 
+      imageUrl: "/books/infinite_jest.jpg",
+      description: "A gargantuan, mind-altering comedy about the pursuit of happiness in America. Set in an addicts' halfway house and a tennis academy, and featuring the most endearingly screwed-up family to come along in recent fiction, Infinite Jest explores essential questions about what entertainment is and why it has come to so dominate our lives; about how our desire for entertainment affects our need to connect with other people; and about what the pleasures we choose say about who we are. ",
+      inventoryQuantity: 10
+  },
+  {
+      title: "Ulysses",
+      author: "James Joyce",
+      genre: ["Novel", "Fiction"],
+      price: 7, 
+      imageUrl: "/books/ulysses.jpg",
+      description: "Ulysses is a novel by Irish writer James Joyce. It was first serialised in parts in the American journal The Little Review from March 1918 to December 1920, and then published in its entirety by Sylvia Beach in February 1922, in Paris. It is considered to be one of the most important works of Modernist literature, and has been called a demonstration and summation of the entire movement. Before Joyce, no writer of fiction had so foregrounded the process of thinking. However, even proponents of Ulysses such as Anthony Burgess have described the book as inimitable, and also possibly mad",
+      inventoryQuantity: 10
+  },
+  {
+      title: "1984",
+      author: "George Orwell",
+      genre: ["Utopian and dystopian fiction", "Social Science Fiction", "Political Fiction"],
+      price: 5, 
+      imageUrl: "/books/1984.jpg",
+      description: "Written in 1948, 1984 was George Orwell’s chilling prophecy about the future. And while 1984 has come and gone, his dystopian vision of a government that will do anything to control the narrative is timelier than ever...",
+      inventoryQuantity: 10
+  },
+  {
+      title: "One Hundred Years of Solitude",
+      author: "Gabriel Garcia Marquez",
+      genre: ["Novel", "Magical Realism"],
+      price: 13, 
+      imageUrl: "/books/hundred_years.jpg",
+      description: "One of the twentieth century’s most beloved and acclaimed novels, One Hundred Years of Solitude tells the story of the rise and fall, birth and death of the mythical town of Macondo through the history of the Buendia family. Inventive, amusing, magnetic, sad, and alive with unforgettable men and women—brimming with truth, compassion, and a lyrical magic that strikes the soul—this novel is a masterpiece in the art of fiction.",
+      inventoryQuantity: 10
+  }
+
+
+]
+
+const syncDb = () => db.sync({ force: true })
+    .then(() => {
+        return Bluebird.map(books, book => {
+            return Book.create(book)
+        })
+    })
+    
 
 // This evaluates as true when this file is run directly from the command line,
 // i.e. when we say 'node server/index.js' (or 'nodemon server/index.js', or 'nodemon server', etc)
@@ -105,3 +168,13 @@ if (require.main === module) {
 } else {
   createApp()
 }
+
+
+
+
+
+
+
+
+
+
