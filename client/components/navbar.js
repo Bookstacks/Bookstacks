221fcacdad71ec091cdb6a48e3d58e4e55c48403
Jacob Feinberg <JacobFeinberg92@gmail.com>
2018-06-10 00:28:09 -0400

    update(navbar-component) added badge to my cart in navbar

--- client/components/navbar.js
+++ client/components/navbar.js
@@ -6,13 +6,15 @@ import {
   NavbarBrand,
   Nav,
   NavItem,
-  NavLink
+  NavLink,
+  Badge
 } from "reactstrap";
 import PropTypes from "prop-types";
 import { connect } from "react-redux";
 import { Link } from "react-router-dom";
 import { logout } from "../store";
 import AllBooks from "./AllBooks";
+import { fetchCart } from "../store";
 
 class MyNavbar extends React.Component {
   constructor(props) {
@@ -28,8 +30,10 @@ class MyNavbar extends React.Component {
     this.setState({
       collapsed: !this.state.collapsed
     });
+    this.props.loadCart(this.props.userId);
   }
   render() {
+    console.log(this.props.cart.lineItems);
     return (
       <div>
         <Navbar color="light" light>
@@ -40,7 +44,20 @@ class MyNavbar extends React.Component {
           <Collapse isOpen={!this.state.collapsed} navbar>
             <Nav navbar>
               <NavItem>
-                <NavLink href={`/cart/${this.props.userId}`}>My Cart</NavLink>
+                <NavLink href="/allbooks">Books</NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink href={`/cart/${this.props.userId}`}>
+                  My Cart
+                  {this.props.cart.lineItems ? (
+                    <Badge color="secondary">
+                      {this.props.cart.lineItems.reduce((sumOfItems, item) => {
+                        sumOfItems += item.quantity;
+                        return sumOfItems;
+                      }, 0)}
+                    </Badge>
+                  ) : null}
+                </NavLink>
               </NavItem>
               {this.props.isLoggedIn ? (
                 <div>
@@ -79,7 +96,8 @@ class MyNavbar extends React.Component {
 const mapState = state => {
   return {
     isLoggedIn: !!state.user.id,
-    userId: state.user.id
+    userId: state.user.id,
+    cart: state.cart
   };
 };
 
@@ -87,6 +105,9 @@ const mapDispatch = dispatch => {
   return {
     handleClick() {
       dispatch(logout());
+    },
+    loadCart: userId => {
+      dispatch(fetchCart(userId));
     }
   };
 };
