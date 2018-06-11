b0a685baa66240822cb4398d0bd7bbf08c3a3adf
Jacob Feinberg <JacobFeinberg92@gmail.com>
2018-06-10 21:52:13 -0400

    create(reviews-component) using bookcard to set up reviews component

---
+++ client/components/Reviews.js
@@ -0,0 +1,76 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { fetchReviews, fetchBook } from "../store";
+import { connect } from "react-redux";
+import {
+  Card,
+  CardImg,
+  CardText,
+  CardBody,
+  CardTitle,
+  CardSubtitle,
+  Button,
+  Container,
+  Col,
+  Row
+} from "reactstrap";
+
+const Reviews = props => {
+  return (
+    <Card body outline color="secondary">
+      <CardBody style={{padding: ".5rem"}}>
+        <CardTitle>{props.book.title}</CardTitle>
+        <CardSubtitle>by {props.book.author}</CardSubtitle>
+      </CardBody>
+      <BookModal book={props.book} addToCart={props.handleClick}/>
+      <CardBody>
+        <Link to={`/books/${props.book.id}`}>
+          <CardImg
+            top
+            width="100%"
+            src={props.book.imageUrl}
+            alt="Card image cap"
+            className="book-img"
+          />
+        </Link>
+        {props.item ? (
+          <div>
+            <CardText style={{marginTop: "1rem", marginBottom:".5rem"}}>Price : ${props.item.price}</CardText>
+            <CardText>Quantity: {props.item.quantity}</CardText>
+            <Button name={props.item.id} onClick={props.handleSubtract} value={props.item.quantity}>-</Button>
+            <Button name={props.item.id} onClick={props.handleAdd} value={props.item.quantity}>+</Button>
+            <br />
+            <Button name={props.item.id} onClick={props.handleDelete}>Remove Item</Button>
+          </div>
+        ) : (
+          <div>
+          <CardText style={{marginTop: "1rem", marginBottom:".5rem"}}>Price : ${props.book.price}</CardText>
+          <Button name={props.book.id} onClick={props.handleClick}>
+            ADD TO CART
+          </Button>
+          </div>
+        )}
+      </CardBody>
+    </Card>
+  );
+};
+
+const mapStateToProps = state => {
+    return {
+      reviews: state.reviews,
+      book: state.singleBook,
+      userId: state.user.id
+    };
+  };
+  
+  const mapDispatchToProps = dispatch => {
+    return {
+      loadAllReviews: dispatch(fetchReviews()),
+      fetchBook: id => dispatch(fetchBook(id))
+    };
+  };
+  
+  export default connect(
+    mapStateToProps,
+    mapDispatchToProps
+  )(Reviews);
