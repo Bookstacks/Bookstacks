ff5e5191e6cfef28d33af713760e0d61633efdad
Jacob Feinberg <37545863+JTFeinberg@users.noreply.github.com>
2018-06-12 23:41:17 -0400

    updated home to have past order card (#130)

--- client/components/user-home.js
+++ client/components/user-home.js
@@ -1,50 +1,79 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-import { connect } from 'react-redux'
-import Admin from './Admin'
+import React from "react";
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
+import Admin from "./Admin";
 import { Link } from "react-router-dom";
+import {
+  Card,
+  CardImg,
+  CardText,
+  CardBody,
+  CardTitle,
+  Button,
+  Container,
+  Row,
+  Col
+} from "reactstrap";
 /**
  * COMPONENT
  */
-export const UserHome = (props) => {
-  const { email } = props
+export const UserHome = props => {
+  const { email } = props;
 
   return (
     <div>
-      {/* <h3>Welcome, {email}</h3> */}
-      <Link to = "/order"> Past Orders </Link>
-    {
-      props.isAdmin ? (
+      {props.isAdmin ? (
         <div>
-            <h3>Welcome, {email}</h3>
-            <Admin />
-          </div>
-        
+          <h3>Welcome, {email}</h3>
+          <Admin />
+        </div>
       ) : (
-          <div>
-            <h3>Welcome, {email}</h3>
-          </div>
-        )
-    }
+        <div>
+          <h3>Welcome, {email}</h3>
+          <Container>
+            <Row style={{justifyContent: 'center'}}>
+              <Col xs="6" sm="4">
+                <Card>
+                  <CardImg
+                    top
+                    width="100%"
+                    src="https://cdn2.iconfinder.com/data/icons/wayfinding-system-basic-icon-set/512/salesroom-512.png"
+                    alt="Card image cap"
+                  />
+                  <CardBody>
+                  <CardTitle style={{textAlign: 'center', fontSize: '25pt'}}>Orders</CardTitle>
+                    <CardText style={{margin: '0'}}>Click below to see your order history.</CardText>
+                    <div style={{display: 'flex', justifyContent: 'center'}}>
+                    <Link to="/order">
+                      <Button>View All Past Orders</Button>
+                    </Link>
+                    </div>
+                  </CardBody>
+                </Card>
+              </Col>
+            </Row>
+          </Container>
+        </div>
+      )}
     </div>
-  )
-}
+  );
+};
 
 /**
  * CONTAINER
  */
-const mapState = (state) => {
+const mapState = state => {
   return {
     isAdmin: state.user.admin,
     email: state.user.email
-  }
-}
+  };
+};
 
-export default connect(mapState)(UserHome)
+export default connect(mapState)(UserHome);
 
 /**
  * PROP TYPES
  */
 UserHome.propTypes = {
   email: PropTypes.string
-}
+};
